f0b66f7293bce15d6c7e67d4223de781881f87e2
Tetiana-Shev <azaliya939@gmail.com>
2022-10-14 15:47:09 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,53 +1,45 @@
 import Notiflix from 'notiflix';
 
-let timerId = null;
-let formValues = {};
-
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(e) {
+  e.preventDefault();
+  let delay = Number(document.querySelector('[name="delay"]').value);
+  let step = Number(document.querySelector('[name="step"]').value);
+  let amount = Number(document.querySelector('[name="amount"]').value);
+
+  if (delay >= 0 && step >= 0) {
+    for (let position = 1; position <= amount; position += 1) {
+      createPromise(position, delay)
+        .then(({ position, delay }) =>
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`,
+          )
+        )
+        .catch(({ position, delay }) =>
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`,
+          )
+        );
+      delay += step;
+    }
+  } else {
+    window.alert("Всі значення у полях не повинні бути від'ємними");
+    return;
+  }
+}
+
 function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
-        // Fulfill
-      } else {
-        reject({ position, delay });
-        // Reject
       }
+      reject({ position, delay });
     }, delay);
   });
-}
-
-function onFormSubmit(e) {
-  e.preventDefault();
-
-  const formData = new FormData(e.currentTarget);
-
-  for (let [name, value] of formData) {
-    formValues[name] = value;
-  }
-
-  let delay = Number(formValues.delay);
-  let step = Number(formValues.step);
-  let amount = Number(formValues.amount);
-
-  for (let position = 1; position <= amount; position += 1) {
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
-      });
-
-    delay += step;
-  }
 }
